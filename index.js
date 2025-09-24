c8e1658ac4acaab6430ade0ab93a8e284eed78cb
Filip Jurczyk <f0x3e8@gmail.com>
2025-09-24 06:42:21 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import { createServer } from "node:http";
+import { readFileSync } from "node:fs";
+import { URL } from "node:url";
+
+const index_html = readFileSync("static/index.html");
+
+// Create a HTTP server
+const server = createServer((req, res) => {
+  const request_url = new URL(`http://${host}${req.url}`);
+  const path = request_url.pathname;
+  console.log(`Request: ${req.method} ${path}`);
+
+  if (path === "/") {
+    if (req.method !== "GET") {
+      res.writeHead(405, { "Content-Type": "text/plain" });
+      res.end("Method not allowed\n");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(index_html);
+    }
+  }
+
+  if (path === "/hello") {
+    if (req.method !== "GET") {
+      res.writeHead(405, { "Content-Type": "text/plain" });
+      res.end("Method not allowed\n");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("hello world!\n");
+    }
+  }
+
+  if (!res.writableEnded) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Site not found!\n");
+  }
+});
+
+const port = 8000;
+const host = "localhost";
+
+// Start the server
+server.listen(port, host, () => {
+    console.log(`Server listening on http://${host}:${port}`);
+});
